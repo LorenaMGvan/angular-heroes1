b91115b67324c51c921904a60c4ca08db1d25c3c
LorenaMGvan <8186769+LorenaMGvan@users.noreply.github.com>
2023-04-25 17:55:40 -0500

    agregar tarjeta de heroes

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
@@ -6,6 +10,33 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
 
+  public hero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+    ) {
+  }
+
+  ngOnInit(): void {
+    this.activateRoute.params
+    .pipe(
+      //delay(3000),
+      switchMap( ({ id }) => this.heroService.getHeroById(id ) ),
+    ).subscribe( heroe => {
+
+      if( !heroe ) return this.router.navigate(['/heroes.list']);
+
+      this.hero = heroe;
+      console.log({ heroe });
+      return;
+    })
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list');
+  }
 }
