d5f66d38e31da546e9ff4e024e9e53c6b317e707
LorenaMGvan <8186769+LorenaMGvan@users.noreply.github.com>
2023-04-26 13:39:35 -0500

    agregar CRUD

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { enviroments } from 'src/enviroments/enviroments';
 
@@ -24,6 +24,24 @@ export class HeroService {
     // regresa un observable que emite Valores de tipo HEro como un arreglo
     getSugerencias(query: string): Observable<Hero[]> {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`)
+    }
+
+    addHero(hero: Hero): Observable<Hero> {
+        return this.http.post<Hero>(`${ this.baseUrl }/heroes`, hero);
+    }
+
+    updateHero(hero: Hero): Observable<Hero> {
+        if( !hero.id ) throw Error('El Héroe es requerido');
+        
+        return this.http.patch<Hero>(`${ this.baseUrl }/heroes`, hero);
+    }
+
+    deleteHeroById(id: string): Observable<boolean> {        
+        return this.http.delete(`${ this.baseUrl }/heroes/${ id }`)
+        .pipe(
+            catchError( err => of(false) ),
+            map( resp => true)   // el map sirve para transformar la respuesta
+        )
     }
     
 }
